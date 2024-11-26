f4ced2bfe840ae09a773b9c55fadf47f224de9d4
Darko Živić <zivic.darko79@gmail.com>
2024-11-26 17:51:47 +0100

    "Added HeroSection component translations for sr-Cyrl and sr-Latn locales, and updated HeroSection.tsx to use translations and handle locale-specific links."

--- src/components/mainpage/HeroSection.tsx
+++ src/components/mainpage/HeroSection.tsx
@@ -1,20 +1,30 @@
+"use client"
 import React from "react";
 import { HiArrowRight } from "react-icons/hi2";
 import Image from "next/image";
-
+import { useTranslations } from 'next-intl';
+import { usePathname } from 'next/navigation';
 import { FeaturedImages } from "./FeaturedImages";
+import Link from "next/link";
 
 export default function HeroSection() {
+  const t = useTranslations('HeroSection')
+  const pathname = usePathname();
+  const pathSegments = pathname.split('/');
+  const currentLocale = pathSegments[1] || 'sr-Latn';
+  const localizedPath = `/${currentLocale}${'/kontakt'}`;
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 md:gap-20 max-w-7xl mx-auto pt-20 md:pt-40 relative overflow-hidden px-4 md:px-8">
-      <div className="flex  flex-col items-start">
+      <div className="flex flex-col items-start">
         <h1 className="text-3xl md:text-5xl md:leading-tight max-w-5xl text-left tracking-tight font-bold bg-clip-text text-transparent bg-gradient-to-b dark:from-neutral-800 dark:via-white dark:to-white from-neutral-800 via-neutral-600 to-neutral-600">
-        Make your dashboard 10x more optimized.
-
+       {t('title1')}
+       <span className="uppercase text-4xl gap-1 flex items-center justify-start text-accent dark:text-accentDark">
+        {t('span')}
+        </span>
         </h1>
         <p className=" mt-2 md:mt-6 text-left md:text-xl text-neutral-600 dark:text-neutral-400 max-w-3xl  relative z-10">
-          With our expert dashboard solutions, you&apos;ll have no trouble
-          tracking your leads, campaigns, and more.
+        {t('paragraph')}
         </p>
         <FeaturedImages
           textClassName="lg:text-left text-left"
@@ -23,13 +33,16 @@ export default function HeroSection() {
         />
         <div className="flex flex-col sm:flex-row gap-4 justify-center sm:justify-start w-full items-center">
           <div className="flex items-center gap-4 justify-start my-4 relative z-10">
+            <Link 
+                href={localizedPath}>
             <button className="flex space-x-2 items-center group bg-gradient-to-b from-indigo-500 to-blue-600 px-4 py-2 rounded-2xl text-white shadow-[0px_3px_0px_0px_rgba(255,255,255,0.1)_inset]">
-              <span>Book a demo</span>{" "}
+              <span>{t('button1')}</span>{" "}
               <HiArrowRight className="text-white group-hover:translate-x-1 stroke-[1px] h-3 w-3 mt-0.5 transition-transform duration-200" />
             </button>
+            </Link>
           </div>
-          <button className="text-neutral-700 dark:text-300 text-base font-medium">
-            Know more
+          <button className="text-gray dark:text-300 text-base font-medium">
+          {t('button2')}
           </button>
         </div>
       </div>

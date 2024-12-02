4313739637be925a1a6843b51c1738e0e52b8dc4
Darko Živić <zivic.darko79@gmail.com>
2024-12-02 17:00:29 +0100

    "Update alt text of Image component in HeroSection from 'Dashboard Image' to 'Srpski kulturni centar'"

--- src/components/mainpage/HeroSection.tsx
+++ src/components/mainpage/HeroSection.tsx
@@ -54,7 +54,7 @@ export default function HeroSection() {
         <div className="border border-neutral-200 dark:border-neutral-800 rounded-3xl p-4 bg-neutral-100 dark:bg-neutral-900 shadow-[0px_0px_5px_1px_rgba(0,0,0,0.05)_inset] dark:shadow-[0px_0px_5px_1px_rgba(255,255,255,0.05)_inset]">
           <Image
             src="/banner.jpg"
-            alt="Dashboard Image"
+            alt="Srpski kulturni centar"
             width={1000}
             height={1000}
             className="rounded-2xl h-72 w-full"

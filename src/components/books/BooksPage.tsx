0fb43192eafe0eaf6d52a6e22419b4a48a6da22b
Darko Živić <zivic.darko79@gmail.com>
2024-12-05 17:50:17 +0100

    "Added useRef hook and scrollIntoView functionality to BooksPage component"

--- src/components/books/BooksPage.tsx
+++ src/components/books/BooksPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Pagination from "../pagination/Pagination";
 import SearchBar from "../header/SearchBar";
 import { useTranslations } from 'next-intl';
@@ -30,6 +30,9 @@ const BooksPage = () => {
   const [searchQuery, setSearchQuery] = useState<string>(""); // Drži trenutni termin pretrage
 
   const booksPerPage = 6;
+
+  // Ref za sekciju sa knjigama
+  const booksSectionRef = useRef<HTMLDivElement | null>(null);
 
   const fetchBooks = async (page: number, limit: number, search?: string) => {
     try {
@@ -69,6 +72,15 @@ const BooksPage = () => {
     setCurrentPage(1); // Resetuje paginaciju na prvu stranu
   };
 
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+
+    // Skroluj do sekcije sa knjigama
+    if (booksSectionRef.current) {
+      booksSectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   if (loading) {
     return <p className="text-center text-accent dark:text-accentDark flex items-center justify-center mt-20">
       {t('loading')}
@@ -83,49 +95,49 @@ const BooksPage = () => {
 
   return (
     <>
-    <LibraryIntro />
-    <div className="px-10 md:px-20 my-20">
-      <div className="flex items-center justify-end">
-      <SearchBar onSearch={handleSearch} /> {/* Prosleđuje funkciju pretrage */}
-      </div>
-      <h1 className="flex items-center justify-center text-center text-3xl lg:text-4xl font-bold mb-10 text-purple-700 dark:text-accentDark">
-        {t('title1')} <br/> {t('title2')}
-      </h1>
-      <p className="flex items-center justify-center text-clip text-neutral-800 dark:text-gray">
-        {t('paragraph')}
-      </p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 my-20">
-        {books.map((book) => (
-          <div
-            key={book.id}
-            className="border rounded-xl p-4 shadow-lg shadow-gray dark:shadow-accentDark bg-white dark:bg-gray"
-          >
-            <h2 className="text-xl font-semibold mb-2 text-purple-700">
-              {book.skupina1}
-            </h2>
-            <p className="text-sm text-neutral-800">
-              <strong>{t('godina')}:</strong> {book.godina || "N/A"}
-            </p>
-            <p className="text-sm text-neutral-800">
-              <strong>{t('ISBN')}:</strong> {book.isbn || "N/A"}
-            </p>
-            <p className="text-sm text-neutral-800">
-              <strong>{t('inventurni_broj')}:</strong> {book.invbroj || "N/A"}
-            </p>
-            <p className="text-sm text-neutral-800">
-              <strong>{t('signatura')}:</strong> {book.signatura || "N/A"}
-            </p>
-          </div>
-        ))}
-      </div>
+      <LibraryIntro />
+      <div ref={booksSectionRef} className="px-10 md:px-20 my-20">
+        <div className="flex items-center justify-end">
+          <SearchBar onSearch={handleSearch} /> {/* Prosleđuje funkciju pretrage */}
+        </div>
+        <h1 className="flex items-center justify-center text-center text-3xl lg:text-4xl font-bold mb-10 text-purple-700 dark:text-accentDark">
+          {t('title1')} <br /> {t('title2')}
+        </h1>
+        <p className="flex items-center justify-center text-clip text-neutral-800 dark:text-gray">
+          {t('paragraph')}
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 my-20">
+          {books.map((book) => (
+            <div
+              key={book.id}
+              className="border rounded-xl p-4 shadow-lg shadow-gray dark:shadow-accentDark bg-white dark:bg-gray"
+            >
+              <h2 className="text-xl font-semibold mb-2 text-purple-700">
+                {book.skupina1}
+              </h2>
+              <p className="text-sm text-neutral-800">
+                <strong>{t('godina')}:</strong> {book.godina || "N/A"}
+              </p>
+              <p className="text-sm text-neutral-800">
+                <strong>{t('ISBN')}:</strong> {book.isbn || "N/A"}
+              </p>
+              <p className="text-sm text-neutral-800">
+                <strong>{t('inventurni_broj')}:</strong> {book.invbroj || "N/A"}
+              </p>
+              <p className="text-sm text-neutral-800">
+                <strong>{t('signatura')}:</strong> {book.signatura || "N/A"}
+              </p>
+            </div>
+          ))}
+        </div>
 
-      {/* Pagination */}
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={(newPage) => setCurrentPage(newPage)}
-      />
-    </div>
+        {/* Pagination */}
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
     </>
   );
 };

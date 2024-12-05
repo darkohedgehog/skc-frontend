f4daa72a1ee980b6b06b6ba4069dddf61fd3d7b8
Darko Živić <zivic.darko79@gmail.com>
2024-12-05 14:45:29 +0100

    "Updated translations for sr-Cyrl and sr-Latn languages, and refactored StatutDocument component to use new translations and added react-icon for download button."

--- src/components/statut/StatutDocument.tsx
+++ src/components/statut/StatutDocument.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { StatutType } from "../../../types";
 import Link from "next/link";
+import { FaDownload } from "react-icons/fa";
 
 async function fetchStatut(locale: string): Promise<StatutType | null> {
   try {
@@ -22,6 +23,7 @@ async function fetchStatut(locale: string): Promise<StatutType | null> {
 }
 
 const StatutDocument = () => {
+  const t = useTranslations("Statut");
   const locale = useLocale(); // Dinamički dohvat jezika
   const [statut, setStatut] = useState<StatutType | null>(null);
   const [loading, setLoading] = useState(true);
@@ -39,23 +41,23 @@ const StatutDocument = () => {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center text-accent dark:text-accentDark h-screen">
-        Učitavanje...
+      <div className="flex items-center justify-center text-accent dark:text-accentDark h-screen mt-40">
+        {t('loading')}
       </div>
     );
   }
 
   if (!statut) {
     return (
-      <div className="flex items-center justify-center text-red-500 h-screen">
-        Statut nije pronađen.
+      <div className="flex items-center justify-center text-red-500 h-screen mt-40">
+        {t('available')}
       </div>
     );
   }
 
   return (
-    <div className="px-10 md:px-20 py-10 flex flex-col items-center justify-center">
-      <h1 className="text-2xl lg:text-4xl font-bold text-center text-accent dark:text-accentDark mb-10">
+    <div className="px-10 md:px-20 py-10 flex flex-col items-center justify-center mt-28">
+      <h1 className="text-2xl lg:text-3xl font-bold text-center text-accent dark:text-accentDark mb-10">
         {statut.title}
       </h1>
 
@@ -66,14 +68,16 @@ const StatutDocument = () => {
             download
             target="_blank"
             rel="noopener noreferrer"
-            className="bg-accent text-white px-6 py-3 rounded-lg shadow-lg hover:bg-accentDark w-auto h-auto"
           >
-            Preuzmi dokument
+            <button className="flex space-x-2 items-center group bg-gradient-to-b from-indigo-500 to-blue-600 px-4 py-2 rounded-2xl text-white shadow-[0px_3px_0px_0px_rgba(255,255,255,0.1)_inset]">
+              <span>{t('download')}</span>{" "}
+              <FaDownload className="text-white group-hover:translate-x-1 stroke-[1px] h-3 w-3 mt-0.5 transition-transform duration-200" />
+            </button>
           </Link>
         </div>
       ) : (
         <div className="text-center text-gray-600">
-          Dokument nije dostupan u PDF formatu.
+          {t('content')}
         </div>
       )}
     </div>
